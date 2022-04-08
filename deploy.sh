d4f46b567305da429a2b0cf9a73d31d33d6d5f88
IntegerVector <andrii.ruban@outlook.com>
2022-04-09 00:58:41 +0300

    deploy script aded

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+if [ -z $1 ]
+then
+    echo "You MUST specify target branch!"
+    exit 1
+fi
+if [ -z $2 ]
+then
+    echo "You MUST specify version"
+    exit 1
+fi
+
+echo "Deploying to GitHub pages, to branch $1"
+echo "Build version $2"
+
+set -e
+npm run test
+npm run build
+
+cd dist
+git init
+git branch -M main
+git add -A
+git commit -m "'deploy v$2'"
+git tag "$2"
+git push -f git@github.com:IntegerVector/flash-cards.git main:$1
+git push git@github.com:IntegerVector/flash-cards.git --tags
+cd -
