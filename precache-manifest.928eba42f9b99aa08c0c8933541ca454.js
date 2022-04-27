20c6f93c329268f1d3690ee05bda61477d500a0e
IntegerVector <andrii.ruban@outlook.com>
2022-04-27 23:09:57 +0300

    'deploy v0.6'

---
+++ precache-manifest.928eba42f9b99aa08c0c8933541ca454.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5550f337a8f9ce986b7",
+    "url": "/shitty-reader/css/app.55b1f1c9.css"
+  },
+  {
+    "revision": "086d5375436bf516d66fd763cdca3ca9",
+    "url": "/shitty-reader/index.html"
+  },
+  {
+    "revision": "a5550f337a8f9ce986b7",
+    "url": "/shitty-reader/js/app.45c1f469.js"
+  },
+  {
+    "revision": "fc2edba4d5e6687fcecc",
+    "url": "/shitty-reader/js/chunk-vendors.652cbcf0.js"
+  },
+  {
+    "revision": "bfd18ddea3f94274b8b65848c18f594c",
+    "url": "/shitty-reader/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/shitty-reader/robots.txt"
+  }
+]);
