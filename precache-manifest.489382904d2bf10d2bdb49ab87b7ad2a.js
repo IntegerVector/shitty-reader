9e6a4a094832051954782ba4aedb4c544c47b778
IntegerVector <andrii.ruban@outlook.com>
2022-05-09 22:42:34 +0300

    'deploy v1'

---
+++ precache-manifest.489382904d2bf10d2bdb49ab87b7ad2a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ead10086dec9adbddcd9",
+    "url": "/shitty-reader/css/app.25a9b52c.css"
+  },
+  {
+    "revision": "94b24f2fc723d0a77b7b6125c34e48fa",
+    "url": "/shitty-reader/favicon.png"
+  },
+  {
+    "revision": "468e8e77640cba25b46188cac6afaec6",
+    "url": "/shitty-reader/favicon.svg"
+  },
+  {
+    "revision": "8522aeae76b6a9b06cc4aa02db8cd1d6",
+    "url": "/shitty-reader/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.8522aeae.woff2"
+  },
+  {
+    "revision": "6fb9297099080730d4d0cafcfb9d47b8",
+    "url": "/shitty-reader/index.html"
+  },
+  {
+    "revision": "ead10086dec9adbddcd9",
+    "url": "/shitty-reader/js/app.19546d9a.js"
+  },
+  {
+    "revision": "2676681429665f70f60d",
+    "url": "/shitty-reader/js/chunk-vendors.d21e2a4a.js"
+  },
+  {
+    "revision": "bfd18ddea3f94274b8b65848c18f594c",
+    "url": "/shitty-reader/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/shitty-reader/robots.txt"
+  }
+]);
