758d582e132bf31cb1c0e941a8892b603750a6a1
IntegerVector <andrii.ruban@outlook.com>
2022-05-09 19:39:13 +0300

    Favorites module added

---
+++ src/services/favorites.js
@@ -0,0 +1,29 @@
+import { LocalStorageService } from "./local-storage";
+
+const ls = new LocalStorageService("fav");
+
+export const favorites = {
+  get() {
+    return ls.getData() || [];
+  },
+  add(item) {
+    const fav = this.get();
+    const isExists = this.has(item);
+    if (!isExists) {
+      fav.push(item);
+      ls.saveData(fav);
+    }
+  },
+  remove(item) {
+    const fav = this.get();
+    ls.saveData(
+      fav.filter((element) => {
+        return element !== item;
+      })
+    );
+  },
+  has(item) {
+    const fav = this.get();
+    return !!~fav.indexOf(item);
+  },
+};
